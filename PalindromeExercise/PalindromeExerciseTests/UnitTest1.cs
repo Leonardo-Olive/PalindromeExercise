bc2b6479632fc1aaf6242508cf900af29f78bbdc
leonardo oliveira <leonardo_olivesilva@icloud.com>
2024-02-01 19:37:42 -0800

    Palindrome

--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,20 @@
 using System;
+using PalindromeExercise;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("hello", false)]
+        [InlineData("Madam", true)]
+        public void MyTest(String word, bool expected)
         {
-
+            var test = new WordSmith();
+            var actual = test.IsAPalindrome(word);
+            Assert.Equal(expected, actual);
         }
     }
 }
